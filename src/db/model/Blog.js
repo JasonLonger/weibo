7cfaaf7bc76ab1bce0e72e19bf774c85763f35e8
luojialong <1269152641@qq.com>
2020-03-07 10:32:24 +0800

    feat: 微博用户模型

---
+++ src/db/model/Blog.js
@@ -0,0 +1,31 @@
+/** 
+ * @description 微博数据模型
+ * @author JasonLonger
+*/
+
+const seq = require('../seq');
+const { INTEGER, STRING, TEXT } = require('../types.js');
+
+
+const Blog = seq.define('blog',{
+    userId:{
+        type: INTEGER,
+        allowNull:false,
+        comment: '用户 ID'
+    },
+    content:{
+        type: TEXT,
+        allowNull:false,
+        comment:'微博内容'
+    },
+    image:{
+        type:STRING,
+        comment: '图片地址'
+
+    }
+})
+
+
+module.exports = {
+    Blog
+}
